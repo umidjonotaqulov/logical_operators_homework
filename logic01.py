603af469a042365a94bf515537302211d8fe44a1
umidjonotaqulov <otaqulovumidjon2003@gmail.com>
2022-07-17 16:48:31 +0500

    01

--- logic01.py
+++ logic01.py
@@ -8,8 +8,20 @@ def main(a,b,c):
     Returns:
         bool: answer
     """
-    return a>b and b>c
+    return a>b>c
 
 x=main(9,4,1)
 
 print(x)
+
+def main(a,b,c):
+    """
+    Uchta a, b, c butun sonlar berilgan bo‘lsa, quyidagi “b soni a va c o‘rtasida” degan gapni tekshiring.
+    Args:
+        a(int): a parametri
+        b(int): b parametri
+        c(int): c parametri
+    Qaytaradi:
+        bool: javob
+    """
+    qaytish
